d6722946ada62cf0244733e4e60e4b8f06f15416
GlenCorreia <glen_correia@outlook.com>
2020-07-19 10:58:46 +0000

    Set up Axios to use with Vue

--- noteapp-front/src/main.js
+++ noteapp-front/src/main.js
@@ -5,9 +5,15 @@ import App from './App'
 import router from './router'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-// import VueAxios from 'vue-axios'
+import VueAxios from 'vue-axios'
+import { securedAxiosInstance, plainAxiosInstance } from './backend/axios'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAxios, {
+  secured: securedAxiosInstance,
+  plain: plainAxiosInstance
+})
 
 /* eslint-disable no-new */
 new Vue({
